043dfd9aaaf9769a13b4eee22b60ec3e630314f2
mkakati <mrigangakakati@gmail.com>
2017-07-09 12:54:25 -0700

    airline reservation system using java
    A desktop application designed using java by which easily a user can
    book flight tickets for various destination.

---
+++ airline/src/airline/Airline.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airline;
+
+/**
+ *
+ * @author dell
+ */
+public class Airline {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
